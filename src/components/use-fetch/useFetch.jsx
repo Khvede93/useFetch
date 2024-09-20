ed61e56e4c1ca2930a6f8d9cedf0f78bde9685ad
Khvede93 <gio.khvede93@gmail.com>
2024-09-20 18:43:15 +0200

    useFetch custom hook is ready for testing

--- src/components/use-fetch/useFetch.jsx
+++ src/components/use-fetch/useFetch.jsx
@@ -1,7 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export const useFetch = (url, options = {}) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+
+  async function fetchData() {
+    setLoading(true);
+    try {
+      const response = await fetch(url, { ...options });
+      if (!response.ok) {
+        throw new Error(`Something Went Wrong ! ${response.statusText}`);
+      }
+      const result = await response.json();
+      setData(result);
+      setError(null);
+    } catch (e) {
+      setError(`Something Went Wrong ! ${e}`);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [url]);
+
+  return { data, error, loading };
 };
