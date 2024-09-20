8bd11fe34ded931cf1bf6f903d5b7d66bafb7ad5
Khvede93 <gio.khvede93@gmail.com>
2024-09-20 16:16:55 +0200

    temp useFetch custom hook, add temp states

---
+++ src/components/use-fetch/useFetch.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+
+export const useFetch = (url, options = {}) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+};
