21b352304de21acb6c5da32a11f36ac6d80acf70
Khvede93 <gio.khvede93@gmail.com>
2024-09-20 18:58:46 +0200

    useFetch custom hook is Done

--- src/components/use-fetch/UseCustomFetch.jsx
+++ src/components/use-fetch/UseCustomFetch.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import useFetch from './useFetch';
+import './styles.css';
 
 export const UseCustomFetch = () => {
   const { data, error, loading } = useFetch(
@@ -10,8 +11,17 @@ export const UseCustomFetch = () => {
   console.log(data, error, loading);
 
   return (
-    <div>
-      <h1>UseFetch Custom Hook</h1>
+    <div className='container'>
+      <h1 className='page-title'>UseFetch Custom Hook</h1>
+      {loading ? <h2>Loading Data ! Please Wait</h2> : null}
+      {error ? <h2>Something Went Wrong !</h2> : null}
+      {data && data.products && data.products.length
+        ? data.products.map((product) => (
+            <h2 className='product-title' key={product.id}>
+              {product.title}
+            </h2>
+          ))
+        : null}
     </div>
   );
 };
